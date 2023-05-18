a0282c7f4802130e4ce247bfc0d385b17a620a75
Lamine Diallo <bah07565@gmail.com>
2023-05-18 15:46:30 +0100

    Mobile homepage integration

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,45 @@
+import { useState } from "react"
 import { NavLink } from "react-router-dom"
 import { navInfo } from "../constant/navLink"
 import logoIcon from "../assets/shared/logo.svg"
+import closeIcon from "../assets/shared/icon-close.svg"
+import hamburgerIcon from "../assets/shared/icon-hamburger.svg"
 
 const Header = () => {
+  const [toogle, setToogle] = useState(false)
+
+  const handleClick = () => {
+    return setToogle((prevToogle) => !prevToogle)
+  }
 
   return (
     <>
-      <header className=" text-tertiary">
+      <header className="relative z-10 flex items-center justify-between pb-8 text-tertiary">
         <img src={logoIcon} className="w-10 h-10 ml-4" alt="Logo" />
 
+        <button className="z-20 m-6" onClick={handleClick}>
+          <img
+            src={toogle ? hamburgerIcon : closeIcon}
+            className="aspect-square"
+            alt="button toogle navBar"
+          />
+        </button>
 
         <nav
+          className={`bg-[#ffffff0a] backdrop-blur-[40.7742px] fixed text-right transition-transform ease-out ${
+            toogle ? "translate-x-full" : "translate-x-0"
+          }`}
         >
-          <div >
+          <div className="flex flex-col gap-8 text-left">
             {navInfo.map((nav) => (
               <NavLink
                 to={nav.link}
                 key={nav.number}
-              
+                className="flex items-center"
               >
-           
+                <span className="mr-3 font-bold">{nav.number}</span>
                 {nav.text}
+                <span className="absolute right-0 w-1 h-8 bar bg-tertiary"></span>
               </NavLink>
             ))}
           </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,25 @@
+import { Link } from "react-router-dom"
+
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="container flex flex-col items-center text-center justify-evenly gap-9">
+      <h3 className="font-BarlowCondensed">SO, YOU WANT TO TRAVEL TO</h3>
+      <h1>SPACE</h1>
+      <p>
+        Let’s face it; if you want to go to space, you might as well genuinely
+        go to outer space and not hover kind of on the edge of it. Well sit
+        back, and relax because we’ll give you a truly out of this world
+        experience!
+      </p>
+      <div className="relative flex items-center justify-center w-40 h-40 rounded-full cursor-pointer bg-tertiary ">
+        <span className="absolute inline-flex w-full h-full rounded-full opacity-75 hover:animate-ping bg-tertiary"></span>
+        <Link
+          to="/destination"
+          className="text-secondary font-Bellefair font-[20px] relative inline-flex rounded-full tracking-widest leading-6"
+        >
+          EXPLORE
+        </Link>
+      </div>
     </div>
   )
 }
