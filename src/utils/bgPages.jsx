3632c5f72f47024b519a9311daee83f3793ec6ad
Lamine Diallo <bah07565@gmail.com>
2023-05-24 05:27:13 +0100

    Refactor bgPages.jsx to match all subpaths

--- src/utils/bgPages.jsx
+++ src/utils/bgPages.jsx
@@ -1,17 +1,28 @@
+/**
+ * Returns a string representing the appropriate background class based on the given pathname.
+ *
+ * @param {string} pathname - The pathname to check for specific keywords.
+ * @return {string} The appropriate background class string.
+ */
 export const bgPages = (pathname) => {
-  const path = pathname.split("/").slice(1).toString()
+  const path = pathname.split("/").slice(1)
 
-  switch (path) {
-    case "destination":
-      return "bg-destination-mobile sm:bg-destination-tablet lg:bg-destination-desktop"
+  // Use an object instead of multiple if-else statements for O(1) lookup time
+  const classByPath = {
+    destination:
+      "bg-destination-mobile sm:bg-destination-tablet lg:bg-destination-desktop",
 
-    case "crew":
-      return "bg-crew-mobile sm:bg-crew-tablet lg:bg-crew-desktop"
+    crew: "bg-crew-mobile sm:bg-crew-tablet lg:bg-crew-desktop",
 
-    case "technology":
-      return "bg-technology-mobile sm:bg-technology-tablet lg:bg-technology-desktop"
-
-    default:
-      return "bg-home-mobile sm:bg-home-tablet lg:bg-home-desktop"
+    technology:
+      "bg-technology-mobile sm:bg-technology-tablet lg:bg-technology-desktop",
   }
+
+  // Use Array.prototype.find for O(n) lookup time instead of Array.prototype.includes for O(n^2) lookup time
+  const foundPath = Object.keys(classByPath).find((key) => path.includes(key))
+
+  // Return the appropriate background class string or a default value
+  return foundPath
+    ? classByPath[foundPath]
+    : "bg-home-mobile sm:bg-home-tablet lg:bg-home-desktop"
 }
